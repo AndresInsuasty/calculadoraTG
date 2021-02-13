a0682a6f294927ccf16a1b0cdddc3475fc931f10
Andres <andresinsuastyd10@gmail.com>
2021-02-12 21:27:54 -0500

    maquetado inicial

---
+++ src/components/Calculator.vue
@@ -0,0 +1,143 @@
+<template>
+  <div>
+    <div>
+      <h1>Calculadora de Proyectos</h1>
+      <h2>Trascender Global</h2>
+    </div>
+    <hr />
+    <b-container class="bv-example-row">
+      <b-row>
+        <b-col>
+          <div>
+            <h6>Cuantos pesos vale un dolar?</h6>
+            <b-input-group size="sm" prepend="$" append=".00">
+              <b-form-input v-model="trm" type="number"></b-form-input>
+            </b-input-group>
+          </div>
+        </b-col>
+
+        <b-col cols="6">
+          <div>
+            <h6>Contribución Deseada: {{ contribucion }}%</h6>
+            <b-input-group prepend="0" append="100" class="mt-2">
+              <b-form-input
+                type="range"
+                min="0"
+                max="100"
+                v-model="contribucion"
+              ></b-form-input>
+            </b-input-group>
+          </div>
+        </b-col>
+
+        <b-col>
+          <div>
+            <h6>Valor a pagar</h6>
+            <b-input-group size="sm" prepend="$" append=".00">
+              <b-form-input type="number" v-model="valorAPagar"></b-form-input>
+            </b-input-group>
+          </div>
+        </b-col>
+      </b-row>
+      <br />
+
+      <b-row>
+        <b-col>
+          <div>
+            <b-dropdown
+              id="dropdown-3"
+              text="De donde viene el proyecto?"
+              variant="primary"
+              class="m-2"
+              v-model="plataforma"
+            >
+              <b-dropdown-item>Freelancer</b-dropdown-item>
+              <b-dropdown-item>Upwork</b-dropdown-item>
+              <b-dropdown-item>Internacional</b-dropdown-item>
+              <b-dropdown-item>Local</b-dropdown-item>
+            </b-dropdown>
+          </div>
+        </b-col>
+
+        <b-col>
+          <div>
+            <b-dropdown
+              id="dropdown-2"
+              text="Moneda del cliente"
+              variant="primary"
+              class="m-2"
+              v-model="monedaCliente"
+            >
+              <b-dropdown-item>Peso Colombiano (COP)</b-dropdown-item>
+              <b-dropdown-item>Dolar Estadounidense (USD)</b-dropdown-item>
+            </b-dropdown>
+          </div>
+        </b-col>
+
+        <b-col>
+          <div>
+            <b-dropdown
+              id="dropdown-1"
+              text="Moneda Trascendentales"
+              variant="primary"
+              class="m-2"
+              v-model="monedaTrascendentales"
+            >
+              <b-dropdown-item>Peso Colombiano (COP)</b-dropdown-item>
+              <b-dropdown-item>Dolar Estadounidense (USD)</b-dropdown-item>
+            </b-dropdown>
+          </div>
+        </b-col>
+      </b-row>
+    </b-container>
+    <hr />
+    <h2>Valor Final</h2>
+    <h6>{{ final_value || 0 }}</h6>
+
+    <b-button variant="danger" type="button" @click="currenciesApi"
+      >enter</b-button
+    >
+  </div>
+</template>
+
+
+
+
+<script>
+import axios from "axios";
+export default {
+  data() {
+    return {
+      trm: 3500,
+      monedaTrascendentales: "COP",
+      valorAPagar: 0,
+      contribucion: 0,
+      monedaCliente: "USD",
+      plataforma: "Freelancer",
+    };
+  },
+  computed: {
+    final_value() {
+      if (this.trm > 0) {
+        return (this.trm * this.contribucion) / 100;
+      }
+      return 0;
+    },
+  },
+  methods: {
+    async currenciesApi() {
+      const output = (
+        await axios.get("https://api.exchangeratesapi.io/latest?base=USD")
+      ).data;
+      console.log(output);
+    },
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.test {
+  border: 5px solid red;
+}
+</style>
