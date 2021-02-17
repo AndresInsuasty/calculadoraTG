2de6b9616dfe052c6ed109876dcbe838377b88a9
Andres <andresinsuastyd10@gmail.com>
2021-02-16 23:25:39 -0500

    arrays de monedas

--- src/components/Calculator.vue
+++ src/components/Calculator.vue
@@ -65,11 +65,8 @@
               id="dropdown-2"
               text="Moneda del cliente"
               variant="primary"
-              class="m-2"
-              v-model="monedaCliente"
-            >
-              <b-dropdown-item>Peso Colombiano (COP)</b-dropdown-item>
-              <b-dropdown-item>Dolar Estadounidense (USD)</b-dropdown-item>
+              class="m-2">
+              <b-dropdown-item v-for="moneda in monedas" :key="moneda.value" @click="monedaCliente=moneda.value">{{moneda.text}}</b-dropdown-item>
             </b-dropdown>
           </div>
         </b-col>
@@ -81,10 +78,13 @@
               text="Moneda Trascendentales"
               variant="primary"
               class="m-2"
-              v-model="monedaTrascendentales"
             >
-              <b-dropdown-item>Peso Colombiano (COP)</b-dropdown-item>
-              <b-dropdown-item>Dolar Estadounidense (USD)</b-dropdown-item>
+              <b-dropdown-item @click="MTCOP"
+                >Peso Colombiano (COP)</b-dropdown-item
+              >
+              <b-dropdown-item @click="MTUSD"
+                >Dolar Estadounidense (USD)</b-dropdown-item
+              >
             </b-dropdown>
           </div>
         </b-col>
@@ -92,7 +92,7 @@
     </b-container>
     <hr />
     <h2>Valor Final</h2>
-    <h6>{{ final_value || 0 }}</h6>
+    <h6>{{ final_value || 0 }} {{monedaCliente}}</h6>
 
     <b-button variant="danger" type="button" @click="currenciesApi"
       >enter</b-button
@@ -111,15 +111,56 @@ export default {
       trm: 3500,
       monedaTrascendentales: "COP",
       valorAPagar: 0,
-      contribucion: 0,
+      contribucion: 60,
       monedaCliente: "USD",
       plataforma: "Freelancer",
+      rates: {},
+      monedas: [
+        {value:'USD',text:'Dolar Estadounidense (USD)'},
+        {value:'EUR',text:'Euros (EUR)'},
+        {value:'GBP',text:'Libra Esterlina (GBP)'},
+        {value:'CAD',text:'Dolar Canadiense (CAD)'},
+        {value:'AUD',text:'Dolar Australiano (AUD)'},
+        {value:'NZD',text:'Dolar Neozelandes (NZD)'},
+        {value:'SGD',text:'Dolar Singapur (SGD)'},
+        {value:'MXN',text:'Peso Mexicano (MXN)'},
+        {value:'COP',text:'Peso Colombiano (COP)'},
+      ],
     };
   },
   computed: {
     final_value() {
+      let final = this.valorAPagar / (1 - parseInt(this.contribucion) / 100);
+      switch (this.monedaCliente) {
+        case "USD":
+          final = final/this.trm;
+          break;
+        case "EUR":
+          final = (final/this.trm)*parseFloat(this.rates.EUR)
+          break;
+        case "GBP":
+          final = (final/this.trm)*parseFloat(this.rates.GBP)
+          break;
+        case "CAD":
+          final = (final/this.trm)*parseFloat(this.rates.CAD)
+          break;
+        case "AUD":
+          final = (final/this.trm)*parseFloat(this.rates.AUD)
+          break;
+        case "NZD":
+          final = (final/this.trm)*parseFloat(this.rates.NZD)
+          break;
+        case "SGD":
+          final = (final/this.trm)*parseFloat(this.rates.SGD)
+          break;
+        case "MXN":
+          final = (final/this.trm)*parseFloat(this.rates.MXN)
+          break;
+
+      }
+
       if (this.trm > 0) {
-        return (this.trm * this.contribucion) / 100;
+        return final.toFixed(0);
       }
       return 0;
     },
@@ -129,7 +170,16 @@ export default {
       const output = (
         await axios.get("https://api.exchangeratesapi.io/latest?base=USD")
       ).data;
-      console.log(output);
+      console.log(output.rates);
+      this.rates = output.rates;
+    },
+
+    MTCOP() {
+      this.monedaTrascendentales = "COP";
+    },
+
+    MTUSD() {
+      this.monedaTrascendentales = "USD";
     },
   },
 };
