5f66f51a89b8b9eb4f6f8e1f3101decb7eefbca6
Andres <andresinsuastyd10@gmail.com>
2021-02-17 10:46:25 -0500

    funcionando ok

--- src/components/Calculator.vue
+++ src/components/Calculator.vue
@@ -49,12 +49,13 @@
               text="De donde viene el proyecto?"
               variant="primary"
               class="m-2"
-              v-model="plataforma"
             >
-              <b-dropdown-item>Freelancer</b-dropdown-item>
-              <b-dropdown-item>Upwork</b-dropdown-item>
-              <b-dropdown-item>Internacional</b-dropdown-item>
-              <b-dropdown-item>Local</b-dropdown-item>
+              <b-dropdown-item
+                v-for="p in plataformasProyectos"
+                :key="p.value"
+                @click="plataforma = p.value"
+                >{{ p.text }}</b-dropdown-item
+              >
             </b-dropdown>
           </div>
         </b-col>
@@ -65,8 +66,14 @@
               id="dropdown-2"
               text="Moneda del cliente"
               variant="primary"
-              class="m-2">
-              <b-dropdown-item v-for="moneda in monedas" :key="moneda.value" @click="monedaCliente=moneda.value">{{moneda.text}}</b-dropdown-item>
+              class="m-2"
+            >
+              <b-dropdown-item
+                v-for="moneda in monedas"
+                :key="moneda.value"
+                @click="monedaCliente = moneda.value"
+                >{{ moneda.text }}</b-dropdown-item
+              >
             </b-dropdown>
           </div>
         </b-col>
@@ -92,10 +99,10 @@
     </b-container>
     <hr />
     <h2>Valor Final</h2>
-    <h6>{{ final_value || 0 }} {{monedaCliente}}</h6>
+    <h3>{{ final_value || 0 }} {{ monedaCliente }}</h3>
 
-    <b-button variant="danger" type="button" @click="currenciesApi"
-      >enter</b-button
+    <b-button variant="success" type="button" @click="currenciesApi"
+      >Consultar Tasas de Cambio</b-button
     >
   </div>
 </template>
@@ -113,51 +120,72 @@ export default {
       valorAPagar: 0,
       contribucion: 60,
       monedaCliente: "USD",
-      plataforma: "Freelancer",
+      plataforma: 0.2,
       rates: {},
       monedas: [
-        {value:'USD',text:'Dolar Estadounidense (USD)'},
-        {value:'EUR',text:'Euros (EUR)'},
-        {value:'GBP',text:'Libra Esterlina (GBP)'},
-        {value:'CAD',text:'Dolar Canadiense (CAD)'},
-        {value:'AUD',text:'Dolar Australiano (AUD)'},
-        {value:'NZD',text:'Dolar Neozelandes (NZD)'},
-        {value:'SGD',text:'Dolar Singapur (SGD)'},
-        {value:'MXN',text:'Peso Mexicano (MXN)'},
-        {value:'COP',text:'Peso Colombiano (COP)'},
+        { value: "USD", text: "Dolar Estadounidense (USD)" },
+        { value: "EUR", text: "Euros (EUR)" },
+        { value: "GBP", text: "Libra Esterlina (GBP)" },
+        { value: "CAD", text: "Dolar Canadiense (CAD)" },
+        { value: "AUD", text: "Dolar Australiano (AUD)" },
+        { value: "NZD", text: "Dolar Neozelandes (NZD)" },
+        { value: "SGD", text: "Dolar Singapur (SGD)" },
+        { value: "MXN", text: "Peso Mexicano (MXN)" },
+        { value: "COP", text: "Peso Colombiano (COP)" },
+      ],
+      plataformasProyectos: [
+        { value: 0.15, text: "Freelancer Recruiter" },
+        { value: 0.1, text: "Freelancer Normal" },
+        { value: 0.2, text: "Upwork 20%" },
+        { value: 0.1, text: "Upwork 10%" },
+        { value: 0.05, text: "Upwork 5%" },
+        { value: 0.05, text: "Internacional PayPal" },
+        { value: 0.03, text: "Internacional Payoneer" },
       ],
     };
   },
   computed: {
     final_value() {
-      let final = this.valorAPagar / (1 - parseInt(this.contribucion) / 100);
+      let final = 0;
+      if (this.monedaTrascendentales == "USD") {
+        final = this.valorAPagar * this.trm;
+      }
+      else{
+        final = this.valorAPagar
+      }
+
+      final = final / (1 - parseInt(this.contribucion) / 100);
+
       switch (this.monedaCliente) {
         case "USD":
-          final = final/this.trm;
+          final = final / this.trm;
           break;
         case "EUR":
-          final = (final/this.trm)*parseFloat(this.rates.EUR)
+          final = (final / this.trm) * parseFloat(this.rates.EUR);
           break;
         case "GBP":
-          final = (final/this.trm)*parseFloat(this.rates.GBP)
+          final = (final / this.trm) * parseFloat(this.rates.GBP);
           break;
         case "CAD":
-          final = (final/this.trm)*parseFloat(this.rates.CAD)
+          final = (final / this.trm) * parseFloat(this.rates.CAD);
           break;
         case "AUD":
-          final = (final/this.trm)*parseFloat(this.rates.AUD)
+          final = (final / this.trm) * parseFloat(this.rates.AUD);
           break;
         case "NZD":
-          final = (final/this.trm)*parseFloat(this.rates.NZD)
+          final = (final / this.trm) * parseFloat(this.rates.NZD);
           break;
         case "SGD":
-          final = (final/this.trm)*parseFloat(this.rates.SGD)
+          final = (final / this.trm) * parseFloat(this.rates.SGD);
           break;
         case "MXN":
-          final = (final/this.trm)*parseFloat(this.rates.MXN)
+          final = (final / this.trm) * parseFloat(this.rates.MXN);
           break;
-
       }
+      final = final / (1 - this.plataforma);
+
+      //divisas
+      final = final / (1 - 0.25);
 
       if (this.trm > 0) {
         return final.toFixed(0);
